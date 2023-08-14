f1b2646e4ac7ac5bb58485e007892ecf64f5685e
gzigue <gabriel.zigue@gmail.com>
2023-08-14 17:28:55 -0300

    docs: Adding Log4j logs to the tests

--- src/test/java/actions/LogParserActions.java
+++ src/test/java/actions/LogParserActions.java
@@ -6,6 +6,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 
 import com.quakelogparser.Game;
@@ -15,23 +16,31 @@ public class LogParserActions extends LogParser{
 	
 	private String filePath = "src\\main\\resources\\qgames.log";
 	private LinkedHashMap<String, Game> gameStats;
+    private Logger logger;
 	
-	public LogParserActions() {}
+	public LogParserActions(Logger logger) {
+		this.logger = logger;
+	}
 	
 	public LinkedHashMap<String, Game> getGameStatsFromLogFile() {
 		String relevantLogs = filterLog(filePath);
 		ArrayList<String> listOfGames = separateByGame(relevantLogs);
 
+		logger.info("Getting the games' stats from the log.");
 		return calculateGameStats(listOfGames);
 	}
 	
 	public int getTotalKillsFromGame1() {
 		gameStats = getGameStatsFromLogFile();
-		return gameStats.get("game_1").getTotal_kills();
+		logger.info("Getting the total number of kills on Game 1");
+		int totalKills = gameStats.get("game_1").getTotal_kills();
+		logger.info("Total number of kills on Game 1: " + totalKills);
+		return totalKills;
 	}
 	
 	public void verifyZeroTotalKillsOnGame1() {
-		Assert.assertEquals(getTotalKillsFromGame1(), 0, 
+		int totalKills = getTotalKillsFromGame1();
+		Assert.assertEquals(totalKills, 0, 
 				"The total number of kills on Game 1 should be 0, but is actually " +
 				getTotalKillsFromGame1());
 	}
@@ -40,6 +49,7 @@ public class LogParserActions extends LogParser{
 		gameStats = getGameStatsFromLogFile();
 		Set<String> statsSet = gameStats.keySet();
 		
+		logger.info("Looping through all the games to search for duplicate players");
 		for(String gameName : statsSet) {
 			Game game = gameStats.get(gameName);
 			ArrayList<String> players = game.getPlayers();
@@ -47,6 +57,7 @@ public class LogParserActions extends LogParser{
 			Set<String> uniquePlayers = new HashSet<String>(players);
 			Assert.assertEquals(players.size(), uniquePlayers.size(),
 					"There is a duplicate player in " + gameName);
+			logger.info(gameName + ": No duplicate players");
 		}
 	}
 	
@@ -57,9 +68,13 @@ public class LogParserActions extends LogParser{
 		gameStats = calculateGameStats(listOfGames);
 		
 		int index = 1;
+		logger.info("Looping through all games tocompare the number of kills with the log");
 		for(String game : listOfGames) {
 			int numberOfKills = game.split("Kill:").length-1;
 			int totalKills = gameStats.get("game_"+index).getTotal_kills();
+			logger.info("game_" + index + 
+					" - Kills on the log: " + numberOfKills + 
+					" - total_kills: " + totalKills);
 			
 			Assert.assertEquals(numberOfKills, totalKills,
 					"The total kills differs from log in game_"+index);
@@ -71,6 +86,8 @@ public class LogParserActions extends LogParser{
 		gameStats = getGameStatsFromLogFile();
 		Set<String> statsSet = gameStats.keySet();
 		
+		logger.info("Looping through all games to compare the number of kills on "
+				+ "kills_by_means with total_kills");
 		for(String gameName : statsSet) {
 			Game game = gameStats.get(gameName);
 			LinkedHashMap<String, Integer> killsByMeans = game.getKills_by_means();
@@ -81,6 +98,8 @@ public class LogParserActions extends LogParser{
 				totalKillsByMeans += killsByMeans.get(mean);
 			}
 			int totalKills = game.getTotal_kills();
+			logger.info(gameName + " - kills_by_means: " + totalKillsByMeans +
+					" - total_kills: " + totalKills);
 			
 			Assert.assertEquals(totalKillsByMeans, totalKills,
 					"Kills by Means is not being calculated correctly in " + gameName);
@@ -96,11 +115,13 @@ public class LogParserActions extends LogParser{
         Map.Entry<String, Integer> currentEntry = playerRanking.entrySet().iterator().next();
         int currentScore = currentEntry.getValue();
 
+        logger.info("Comparing player scores");
 		for(String player : rankingSet) {
 			int score = playerRanking.get(player);
 			
 			Assert.assertTrue(score <= currentScore, 
 					"Player Ranking is not in descending order");
+			logger.info(currentScore + " >= " + score);
 			
 			currentScore = score;
 		}
--- src/test/java/tests/LogParserTests.java
+++ src/test/java/tests/LogParserTests.java
@@ -1,29 +1,37 @@
 package tests;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import actions.LogParserActions;
 
 public class LogParserTests {
 
 	LogParserActions actions;
+    private static final Logger logger = LogManager.getLogger(LogParserTests.class);
 	
 	@Test(description = "The first game has no kills. "
 			+ "This test verifies that the total kills is set as 0")
 	public void verifyZeroTotalKillsOnGameWithNoKills() {
+		logger.info("Starting test verifyZeroTotalKillsOnGameWithNoKills()");
 		actions.verifyZeroTotalKillsOnGame1();
 	}
 	
 	@Test(description = "This test loops through all the games in the log to "
 			+ "assert that no players are duplicated on a game")
 	public void verifyThereAreNoRepeatedPlayers() {
+		logger.info("Starting test verifyThereAreNoRepeatedPlayers()");
 		actions.checkAllGamesForDuplicatedPlayers();
 	};
 	
 	@Test(description = "This test checks if the total kills are being "
 			+ "calculated correctly on all games")
 	public void verifyTotalKillsCalculation() {
+		logger.info("Starting test verifyTotalKillsCalculation()");
 		actions.compareTotalKillsWithLogOnAllGames();
 	}
 	
@@ -31,18 +39,24 @@ public class LogParserTests {
 			+ "dictionary and compares it to the total kills, to make sure "
 			+ "they match")
 	public void verifyKillsByMeansMatchesTotalKills() {
+		logger.info("Starting test verifyKillsByMeansMatchesTotalKills()");
 		actions.compareKillsByMeansWithTotalKillsOnAllGames();
 	}
 	
 	@Test(description = "This test checks if the Player Ranking is being "
 			+ "sorted by descending order of scores")
 	public void verifyPlayerRankingIsOnDescendingOrder() {
+		logger.info("Starting test verifyPlayerRankingIsOnDescendingOrder()");
 		actions.checkIfRankingIsOnDescendingOrder();
 	}
 	
 	@BeforeMethod
 	public void beforeMethod() {
-		actions = new LogParserActions();
+		actions = new LogParserActions(logger);
 	}
 
+	@AfterMethod
+	public void afterMethod() {
+		logger.info("------------------------------");
+	}
 }
