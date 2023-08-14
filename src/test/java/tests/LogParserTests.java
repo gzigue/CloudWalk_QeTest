e43f85647c22c964879b25c53f0638f775762138
gzigue <gabriel.zigue@gmail.com>
2023-08-14 16:37:22 -0300

    docs: Adding descriptions to the test cases

--- src/test/java/tests/LogParserTests.java
+++ src/test/java/tests/LogParserTests.java
@@ -9,27 +9,33 @@ public class LogParserTests {
 
 	LogParserActions actions;
 	
-	@Test
+	@Test(description = "The first game has no kills. "
+			+ "This test verifies that the total kills is set as 0")
 	public void verifyZeroTotalKillsOnGameWithNoKills() {
 		actions.verifyZeroTotalKillsOnGame1();
 	}
 	
-	@Test
+	@Test(description = "This test loops through all the games in the log to "
+			+ "assert that no players are duplicated on a game")
 	public void verifyThereAreNoRepeatedPlayers() {
 		actions.checkAllGamesForDuplicatedPlayers();
 	};
 	
-	@Test
+	@Test(description = "This test checks if the total kills are being "
+			+ "calculated correctly on all games")
 	public void verifyTotalKillsCalculation() {
 		actions.compareTotalKillsWithLogOnAllGames();
 	}
 	
-	@Test
+	@Test(description = "This test gets the sum of kills on the Kills by Means "
+			+ "dictionary and compares it to the total kills, to make sure "
+			+ "they match")
 	public void verifyKillsByMeansMatchesTotalKills() {
 		actions.compareKillsByMeansWithTotalKillsOnAllGames();
 	}
 	
-	@Test
+	@Test(description = "This test checks if the Player Ranking is being "
+			+ "sorted by descending order of scores")
 	public void verifyPlayerRankingIsOnDescendingOrder() {
 		actions.checkIfRankingIsOnDescendingOrder();
 	}
